52f84d66b57e012f2573a70f0334b7c54424901d
Hins Chiu <chiuhi2@oocl.com>
2020-11-27 15:10:39 +0800

    [parkingStory1][HinsChiu]test:implement test for should call ParkingLot parkCar once when call ParkingBoy parkCar given Car, parkingLot, parkingBoy

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,21 @@
 package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
-
+    void should_call_parking_lot_park_car_once_when_parking_boy_park_car_given_parking_lot_parking_boy_car() {
+        //given
+        ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
+        //when
+        parkingBoy.parkCar(car);
+        //then
+        verify(parkingLot, times(1)).parkCar(car);
     }
 }
