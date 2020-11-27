9d9b1aa828a5ffe19f95130361e52c50d9ceb1bd
Hins Chiu <chiuhi2@oocl.com>
2020-11-27 16:02:41 +0800

    [parkingStory1][HinsChiu]feat:implement for should park multiple Car when ParkingLot parkCar given parkingLot capacity three Car two

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,7 +1,28 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+
 public class ParkingLot {
+    private Integer capacity;
+    private HashMap<Ticket, Car> ticketCarMapping;
+
+    ParkingLot(int capacity) {
+        this.capacity = capacity;
+        this.ticketCarMapping = new HashMap<>();
+    }
+
     public Ticket parkCar(Car car) {
-        return new Ticket();
+        if(isFull()) {
+            return null;
+        }
+
+        Ticket ticket = new Ticket();
+        ticketCarMapping.put(ticket, car);
+
+        return ticket;
+    }
+
+    private boolean isFull() {
+        return ticketCarMapping.values().size() == capacity;
     }
 }
