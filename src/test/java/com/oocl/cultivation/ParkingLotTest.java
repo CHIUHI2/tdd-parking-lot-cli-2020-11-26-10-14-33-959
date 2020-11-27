933d619b8a158975c5c1692ec2b7bfd30d0f0344
Hins Chiu <chiuhi2@oocl.com>
2020-11-27 16:04:00 +0800

    [parkingStory1][HinsChiu]test:implement test for should park one Car when ParkingLot parkCar given parkingLot capacity one Car two

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -29,4 +29,18 @@ public class ParkingLotTest {
         assertNotNull(ticket1);
         assertNotNull(ticket2);
     }
+
+    @Test
+    void should_park_one_car_when_parking_lot_park_car_given_parking_lot_capacity_one_car_two() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        //when
+        Ticket ticket1 = parkingLot.parkCar(car1);
+        Ticket ticket2 = parkingLot.parkCar(car2);
+        //then
+        assertNotNull(ticket1);
+        assertNull(ticket2);
+    }
 }
