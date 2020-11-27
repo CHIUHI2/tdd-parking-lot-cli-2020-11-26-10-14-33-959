8202c7709009a489982a876be4495a2ac0a5f890
Hins Chiu <chiuhi2@oocl.com>
2020-11-27 22:12:48 +0800

    [parkingStory2][HinsChiu]testing:update test case for UnrecognizedTicketException

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyTest {
     @Test
-    void should_call_parking_lot_park_car_once_when_parking_boy_park_car_given_parking_lot_parking_boy_car() {
+    void should_call_parking_lot_park_car_once_when_parking_boy_park_car_given_parking_lot_parking_boy_car() throws ParkingLotFullException {
         //given
         ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
         ParkingBoy parkingBoy = new ParkingBoy();
@@ -22,14 +22,13 @@ class ParkingBoyTest {
     }
 
     @Test
-    void should_call_parking_lot_take_car_once_when_parking_boy_take_car_given_parking_lot_parking_boy_ticket() throws UnrecognizedTicketException {
+    void should_call_parking_lot_take_car_once_when_parking_boy_take_car_given_parking_lot_parking_boy_ticket() {
         //given
         ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
         ParkingBoy parkingBoy = new ParkingBoy();
-        Car car = new Car();
+        Ticket ticket = new Ticket();
         //when
-        Ticket ticket = parkingBoy.parkCar(car, parkingLot);
-        parkingBoy.takeCar(ticket, parkingLot);
+        assertThrows(UnrecognizedTicketException.class, () -> parkingBoy.takeCar(ticket, parkingLot));
         //then
         verify(parkingLot, times(1)).takeCar(ticket);
     }
