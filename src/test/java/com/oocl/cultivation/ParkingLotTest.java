7c3e012021ac729013936493d1bacbab8de09813
Hins Chiu <chiuhi2@oocl.com>
2020-11-27 16:01:39 +0800

    [parkingStory1][HinsChiu]test:implement test for should park multiple Car when ParkingLot parkCar given parkingLot capacity three Car two

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -2,13 +2,13 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
     void should_return_ticket_when_parking_lot_park_car_given_parking_lot_car() {
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         //when
         Ticket ticket = parkingLot.parkCar(car);
@@ -17,13 +17,16 @@ public class ParkingLotTest {
     }
 
     @Test
-    void should_park_car_when_parking_lot_park_car_given_parking_lot_car() {
+    void should_park_multiple_car_when_parking_lot_park_car_given_parking_lot_capacity_three_car_two() {
         //given
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(3);
+        Car car1 = new Car();
+        Car car2 = new Car();
         //when
-        Ticket ticket = parkingLot.parkCar(car);
+        Ticket ticket1 = parkingLot.parkCar(car1);
+        Ticket ticket2 = parkingLot.parkCar(car2);
         //then
-        assertNotNull(ticket);
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
     }
 }
