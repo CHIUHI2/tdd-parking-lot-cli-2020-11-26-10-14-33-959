6e7d3b453b37cdca821c4bb7d321504b7dbac9cd
Hins Chiu <chiuhi2@oocl.com>
2020-11-28 20:39:37 +0800

    [parkingStory5][HinsChiu]feat:implement for should call ParkingLot with largest availability rate parkCar once when SuperSmartParkingBoy parkCar given SuperSmartParkingBoy, ParkingLot, Car

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,7 +1,21 @@
 package com.oocl.cultivation;
 
+import java.util.Comparator;
+import java.util.Optional;
+
 public class SuperSmartParkingBoy extends ParkingBoy{
     SuperSmartParkingBoy() {
         super();
+    }
+
+    @Override
+    public Ticket parkCar(Car car) throws ParkingLotFullException {
+        Optional<ParkingLot> availableParkingLotWithHighestAvailabilityRate = super.getManagedParkingLotList().stream()
+                .filter(parkingLot -> !parkingLot.isFull())
+                .max(Comparator.comparing(ParkingLot::getAvailabilityRate));
+
+        if (!availableParkingLotWithHighestAvailabilityRate.isPresent()) throw new ParkingLotFullException();
+
+        return availableParkingLotWithHighestAvailabilityRate.get().parkCar(car);
     }
 }
