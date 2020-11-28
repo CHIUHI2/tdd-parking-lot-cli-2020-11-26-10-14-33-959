d741b0b17166ede9ff0bb19924ba64bae3b7b4b1
Hins Chiu <chiuhi2@oocl.com>
2020-11-28 14:33:23 +0800

    [parkingStory1][HinsChiu]feat:add default capacity for ParkingLot and handle technical issue

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -6,25 +6,32 @@ public class ParkingLot {
     private Integer capacity;
     private HashMap<Ticket, Car> ticketCarMapping;
 
+    ParkingLot() {
+        this.capacity = 10;
+        this.ticketCarMapping = new HashMap<>();
+    }
+
     ParkingLot(int capacity) {
         this.capacity = capacity;
         this.ticketCarMapping = new HashMap<>();
     }
 
     public Ticket parkCar(Car car) {
-        if(isFull()) return null;
+        if(isFull() || car == null || this.ticketCarMapping.values().contains(car)) return null;
 
         Ticket ticket = new Ticket();
-        ticketCarMapping.put(ticket, car);
+        this.ticketCarMapping.put(ticket, car);
 
         return ticket;
     }
 
     public boolean isFull() {
-        return ticketCarMapping.values().size() == capacity;
+        return this.ticketCarMapping.values().size() == this.capacity;
     }
 
     public Car takeCar(Ticket ticket) {
+        if(ticket == null) return null;
+
         Car car = this.ticketCarMapping.get(ticket);
         this.ticketCarMapping.remove(ticket);
 
